2c9da2f0d1e44968bd596815ac86362919529529
4eh711 <Cheh711@gmail.com>
2023-08-27 23:10:04 +0300

    Less_6_Ex.4. Використання фільтрів для попередження

--- create_warn.py
+++ create_warn.py
@@ -1,4 +1,9 @@
 import warnings
 
+warnings.simplefilter("ignore", SyntaxWarning)
+warnings.simplefilter("always", ImportWarning)
+
+
 warnings.warn("Warning! No code here", SyntaxWarning)
+warnings.warn("Warning! Module not import", ImportWarning)
 
